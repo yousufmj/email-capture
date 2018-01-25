e0199591266373bc01766890e670fec867b8e4cf
Yousuf Maalik-Jaleel <ymaalikjaleel@mmw-ymaleekjale.cb.careerbuilder.com>
2018-01-25 21:56:35 +0000

    create file to handle form daa

---
+++ src/emailForm.php
@@ -0,0 +1,27 @@
+<?php
+
+// get db config
+include_once '../config/database.php';
+// get user Class
+include_once '../src/user/user.php';
+
+// initilase db connection
+$database = new Database();
+$db = $database->getConnection();
+
+// Get post data
+$name = strip_tags($_POST['fullname']);
+$email = strip_tags($_POST['email']);
+
+//create new user
+$user = new User($db);
+
+$user->fullName = $name;
+$user->email = $email;
+
+
+if($user->create() === true){
+    echo "Thank you $user->fullName for submitting your details";
+}else{
+    echo "Error: sorry we could not add your details";
+}
