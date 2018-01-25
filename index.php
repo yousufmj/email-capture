06dac129f28a7816c7a626f140c4455c4b06359b
Yousuf Maalik-Jaleel <ymaalikjaleel@mmw-ymaleekjale.cb.careerbuilder.com>
2018-01-25 21:53:00 +0000

    amending form to work with new backend

--- index.php
+++ index.php
@@ -9,16 +9,16 @@
         <div class="row">
             <div class="col-4 align-self-center">
             <h3>Please fill in the below</h3>
-            <form action="" method="POST">
+            <form action="src/emailForm.php" method="POST">
 
                 <div class="form-group">
                     <label for="email">Email address</label>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email">
                 </div>
 
                 <div class="form-group">
                     <label for="fullname">Full Name</label>
-                    <input type="text" class="form-control" id="fullname" placeholder="Full Name">
+                    <input type="text" class="form-control" id="fullname" placeholder="Full Name" name="fullname">
                 </div>
 
                 <button type="submit" class="btn btn-primary">Submit</button>
