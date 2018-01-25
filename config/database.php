72737d1122266f201f7165bfea34c4d51d096c09
Yousuf Maalik-Jaleel <ymaalikjaleel@mmw-ymaleekjale.cb.careerbuilder.com>
2018-01-25 21:21:26 +0000

    create a database connection

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database{
+    // declare db credentials
+    private $host = "localhost";
+    private $dbName = "email_capture";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    // connect to db
+    public function getConnection(){
+        $this->conn = null;
+        try{
+            $this->conn = new PDO("mysql:host=$this->host;dbname=" . $this->dbName, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
