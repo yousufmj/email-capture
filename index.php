27916f21c1b97386d6ba5220deddf1d9ca293d01
Yousuf Maalik-Jaleel <ymaalikjaleel@mmw-ymaleekjale.cb.careerbuilder.com>
2018-01-25 21:06:55 +0000

    create a basic front page

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    </head>
+    <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4 align-self-center">
+            <h3>Please fill in the below</h3>
+            <form action="" method="POST">
+
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+
+                <div class="form-group">
+                    <label for="fullname">Full Name</label>
+                    <input type="text" class="form-control" id="fullname" placeholder="Full Name">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+                </form>
+            </div>
+        </div>
+
+    </body>
+</html>
