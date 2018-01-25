4f2b0e5d1abac1602870e3726b6424fe0823a12d
Yousuf Maalik-Jaleel <ymaalikjaleel@mmw-ymaleekjale.cb.careerbuilder.com>
2018-01-25 21:21:43 +0000

    create a user class

---
+++ src/user/user.php
@@ -0,0 +1,45 @@
+<?php
+
+// connect to DB
+include_once '../config/database.php';
+
+class User{
+    // DB connection
+    private $conn;
+    // properties
+    public function setName($fullName){
+        $this->fullName = $fullName;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    // create new user
+    function createUser($name, $email)
+    {
+        // query to insert new user
+        $query = "INSERT INTO
+                    user(fullname, email)
+                VALUES
+                    (:fullName,
+                    :email)";
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+        // bind values
+        $stmt->bindParam(":fullName", $this->fullName);
+        $stmt->bindParam(":email", $this->email);
+
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }else{
+            return $stmt->errorInfo();
+        }
+    }
+}
